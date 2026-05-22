e95737afe5fd8e8821c9b0c7d781b276520ffdb2
Abdelmomen Moula <abdelmomen.moula@etudiant-enit.utm.tn>
2026-05-22 12:22:06 +0100

    wear drobe add images

--- src/main.tsx
+++ src/main.tsx
@@ -11,8 +11,19 @@ const favicon = document.querySelector<HTMLLinkElement>('link[rel="icon"]') ?? (
   return link;
 })();
 
+const shortcutIcon = document.querySelector<HTMLLinkElement>('link[rel="shortcut icon"]') ?? (() => {
+  const link = document.createElement('link');
+  link.rel = 'shortcut icon';
+  document.head.appendChild(link);
+  return link;
+})();
+
+const iconHref = `${platformLogo}?v=${Date.now()}`;
+
 favicon.type = 'image/png';
-favicon.href = platformLogo;
+favicon.href = iconHref;
+shortcutIcon.type = 'image/png';
+shortcutIcon.href = iconHref;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
